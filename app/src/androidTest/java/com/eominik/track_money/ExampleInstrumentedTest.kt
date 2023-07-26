642cd9a6555081d33b623799e622adb71926be6f
Eominik <dominik.kagw@gmail.com>
2023-07-26 12:16:18 +0200

    Initial commit

---
+++ app/src/androidTest/java/com/eominik/track_money/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.eominik.track_money
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.eominik.track_money", appContext.packageName)
+    }
+}
